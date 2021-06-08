b4ad4881d38455912b12ebd688af191ec2ee75db
martin-moringa <martin.mylles@student.moringachool.com>
2021-06-08 22:53:13 +0300

    add newbase url in app folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from .config import DevConfig
 
-
 # initialising application
 app = Flask(__name__)
+app = Flask(__name__, instance_relative_config = True)
 
 
 # setting up configurations
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from app import views
