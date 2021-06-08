25305ff877b56295608c8e26d6dcafd3b30140cb
martin-moringa <martin.mylles@student.moringachool.com>
2021-06-08 23:18:24 +0300

    Defines a function that fetches articles and returns articles object

--- app/request.py
+++ app/request.py
@@ -6,6 +6,11 @@ from .models import news
 apiKey = app.config['NEWS_API_KEY']
 # getting the movie base url
 base_url = app.config["NEWS_API_BASE_URL"]
+
+
+
+# .........................get news section............................
+
 def get_news(category):
     '''
     Function that gets the json response to our url request
@@ -21,9 +26,6 @@ def get_news(category):
             news_results_list = get_news_response["sources"]
             news_results = process_results(news_results_list)
     return news_results
-
-
-
 def process_results(news_list):
     '''
     Function  that processes the news result and transform them to a list of Objects
@@ -41,8 +43,65 @@ def process_results(news_list):
         category = news_item.get('category')
         language = news_item.get('language')
         country = news_item.get('country')
-
+        
         # news_object = news(id,name,description,url,category,country,language)
         # news_results.append(news_object)
-
     return news_results
+
+
+    # ..........................end of get news...................................
+
+def get_articles(id):
+    '''
+    function that processes the articles and returns a list of articles objects
+    '''
+    get_articles_url = articles_url.format(id,apiKey)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+    articles_results = json.loads(url.read())
+
+    articles_object = None
+    if articles_results['articles']:
+        articles_object = process_articles(articles_results['articles'])
+
+	return articles_object
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def process_results(news_list):
+#     '''
+#     Function  that processes the movie result and transform them to a list of Objects
+
+#     Args:
+#         movie_list: A list of dictionaries that contain movie details
+
+#     Returns :
+#         movie_results: A list of movie objects
+#     '''
+#     news_results = []
+#     for news_item in news_list:
+#         name = news_item.get('name')
+#         title = news_item.get('title')
+#         author = news_item.get('author')
+#         title = news_item.get('title')
+#         description = news_item.get('description')
+#         url = news_item.get('url')
+#         urlToImage = news_item.get('urlToImage')
+#         publishedAt = news_item.get('publishedAt')
+#         content = news_item.get('content')
+
+#         # if poster:
+#         #     news_object = news(name,author,title,description,url,urlToImage,publishedAt,content)
+#         #     news_results.append(news_object)
+
+#     return news_results 
